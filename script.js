0f656483b9365dce10eb14533b28ebf425fb6679
ankush0710 <ankushkurvey053@gmail.com>
2026-01-23 17:11:35 +0530

    some changes done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const scroll = ScrollReveal({
     reset: true,
-    duration:2600,
+    duration:2000,
     distance:'60px'
 })
 
-scroll.reveal('.image-transform',{delay:400, origin:'top'})
-scroll.reveal('.text-transform',{delay:300, origin:'left'})
+scroll.reveal('.image-transform',{delay:300, origin:'top'})
+scroll.reveal('.text-transform',{delay:200, origin:'left'})
